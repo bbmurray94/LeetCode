0a62c0ab4e6c1c228d33d43280d5bec1ba7537e7
Beck Murray <Beck.Murray@hyland.com>
2023-05-22 10:59:32 -0400

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,27 @@
+﻿namespace FizzBuzz
+{
+    internal static class FizzBuzz
+    {
+        public static IList<string> Solve(int n) 
+        {
+            IList<string> resultList = new List<string>();
+
+            for (int i = 1; i <= n; i++) 
+            {
+                string result = string.Empty;
+                if (i % 3 == 0) 
+                {
+                    result += "Fizz";
+                }
+                if (i % 5 == 0) 
+                {
+                    result += "Buzz";
+                }
+
+                resultList.Add(!string.IsNullOrEmpty(result) ? result : i.ToString());
+            }
+
+            return resultList;
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿using FizzBuzz;
+using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        FizzBuzz.FizzBuzz.Solve(3).ToList().ForEach(i => Console.Write("{0} ", i));
+        Console.WriteLine();
+        FizzBuzz.FizzBuzz.Solve(5).ToList().ForEach(i => Console.Write("{0} ", i));
+        Console.WriteLine();
+        FizzBuzz.FizzBuzz.Solve(15).ToList().ForEach(i => Console.Write("{0} ", i));
+    }
+}
