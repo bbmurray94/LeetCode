639c96b6e0f22df81c9b245a8fd929cfc98649df
Ben Murray <beckmurray94@gmail.com>
2023-05-22 15:01:55 -0400

    Roman To Int

---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(RomanToInteger.RomanToInteger.Convert("III"));
+Console.WriteLine(RomanToInteger.RomanToInteger.Convert("IV"));
+Console.WriteLine(RomanToInteger.RomanToInteger.Convert("MCMXCIV"));
+
---
+++ RomanToInteger/RomanToInteger.cs
@@ -0,0 +1,40 @@
+﻿
+namespace RomanToInteger
+{
+    internal static class RomanToInteger
+    {
+        private static readonly Dictionary<char, int> pairs = new Dictionary<char, int>
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+        
+        public static int Convert(string s) 
+        {
+            int result = 0;
+            char[] charArray = s.ToCharArray().Reverse().ToArray();
+            int previousValue = 0;    
+
+            foreach (char value in charArray) 
+            {
+                int currentValue = pairs[value];
+                if (currentValue < previousValue)
+                {
+                    result -= currentValue;
+                }
+                else 
+                {
+                    result += currentValue;
+                }
+                previousValue = currentValue;
+            }
+
+            return result;
+        }
+    }
+}
